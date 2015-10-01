506ba62df08a44602f16fffeaa7be7101ad33b59
Chris Cowan <agentme49@gmail.com>
2015-10-01 11:17:03 -0700

    use let and const because flow supports them now

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 /* @flow */
 //jshint ignore:start
 
-var Kefir = require('kefir');
+const Kefir = require('kefir');
 import {defonce} from 'ud';
 
 export default function udKefir<T>(module: typeof module, value: T, key?:string=''): Kefir.Stream<T> {
-  var sharedObject = defonce(module, () => {
-    var maybeEmitter;
-    var stream = Kefir.stream(_emitter => {
+  const sharedObject = defonce(module, () => {
+    let maybeEmitter;
+    const stream = Kefir.stream(_emitter => {
       maybeEmitter = _emitter;
     });
-    var property = stream.toProperty();
+    const property = stream.toProperty();
     // force the property to be active to set and remember its current value.
     property.onValue(()=>{});
 
     if (!maybeEmitter) throw new Error("Should not happen, emitter was not set");
-    var emitter = maybeEmitter;
+    const emitter = maybeEmitter;
 
     return {
       property,
